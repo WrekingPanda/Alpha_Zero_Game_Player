ddc3c2565efe7331e44794a75747e075756ea305
anfisou <anfisou03@gmail.com>
2024-01-12 02:48:24 +0000

    Update attax6x6.py

--- attax6x6.py
+++ attax6x6.py
@@ -4,7 +4,7 @@ from ataxx import AttaxxBoard
 from torch.optim import Adam
 
 A6_MODEL_PARAMS = {"size":6, "action_size":6**4, "num_resBlocks":20, "num_hidden":64} 
-A6_TRAIN_PARAMS = {"n_iterations":20, "self_play_iterations":100, "mcts_iterations":200, "n_epochs":20, "batch_size":128, "n_self_play_parallel":20, "move_cap":6**3}
+A6_TRAIN_PARAMS = {"n_iterations":20, "self_play_iterations":100, "mcts_iterations":150, "n_epochs":20, "batch_size":128, "n_self_play_parallel":20, "move_cap":6**3}
 
 def train():
     model = Net(**A6_MODEL_PARAMS)
