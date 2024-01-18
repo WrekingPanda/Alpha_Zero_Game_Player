924260a398288ad58225c23d70f8ffc1181af4e9
anfisou <anfisou03@gmail.com>
2024-01-18 12:37:07 +0000

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -12,7 +12,7 @@ class Net(nn.Module):
 
         # Initial block: Convolution + Batch Normalization + ReLU
         self.startBlock = nn.Sequential(
-            nn.Conv2d(3, num_hidden, kernel_size=size, padding='same'),
+            nn.Conv2d(3, num_hidden, kernel_size=3, padding='same'),
             nn.BatchNorm2d(num_hidden),
             nn.ReLU()
         )
@@ -24,7 +24,7 @@ class Net(nn.Module):
         
         # Policy head: Convolution + Batch Normalization + ReLU + Flatten + Linear + Softmax
         self.policyHead = nn.Sequential(
-            nn.Conv2d(num_hidden, 32, kernel_size=size, padding='same'),
+            nn.Conv2d(num_hidden, 32, kernel_size=3, padding='same'),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Flatten(),
@@ -34,7 +34,7 @@ class Net(nn.Module):
         
         # Value head: Convolution + Batch Normalization + ReLU + Flatten + Linear + Tanh
         self.valueHead = nn.Sequential(
-            nn.Conv2d(num_hidden, size, kernel_size=size, padding='same'),
+            nn.Conv2d(num_hidden, size, kernel_size=3, padding='same'),
             nn.BatchNorm2d(size),
             nn.ReLU(),
             nn.Flatten(),
@@ -58,9 +58,9 @@ class ResBlock(nn.Module):
     def __init__(self, num_hidden, size):
         super().__init__()
         # Residual block: Convolution + Batch Normalization + ReLU + Convolution + Batch Normalization
-        self.conv1 = nn.Conv2d(num_hidden, num_hidden, kernel_size=size, padding='same')
+        self.conv1 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding='same')
         self.bn1 = nn.BatchNorm2d(num_hidden)
-        self.conv2 = nn.Conv2d(num_hidden, num_hidden, kernel_size=size, padding='same')
+        self.conv2 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding='same')
         self.bn2 = nn.BatchNorm2d(num_hidden)
         
     def forward(self, x):
